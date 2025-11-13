572bf17f99d46951d38f7cf9e81bf081a6911a48
AKANA SIGNING JOSIAS AARON <josiasange37@gmail.com>
2025-11-13 09:27:20 +0100

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://nb-dance-award.vercel.app,
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nb-dance-award.vercel.app/candidats',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nb-dance-award.vercel.app/classement',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nb-dance-award.vercel.app/regles',
+      lastModified: new Date(),
+    },
+  ]
+}
