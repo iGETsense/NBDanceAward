574c5857b720d79ac6e25d6b67c2849f0f80a9f8
TSANGA-AWANA <tsangaawanaeugenegontran@gmail.com>
2025-12-04 06:09:32 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import NBDanceAwardPage from "./PageContent"
 export default function Page() {
     return (
         <Suspense fallback={<div className="min-h-screen bg-black flex items-center justify-center">
-            <div className="text-white">Chargement...</div>
+            <div className="text-white">Site en maintenance ....\n mercie de bien vouloir patienter</div>
         </div>}>
-            <NBDanceAwardPage />
+            //<NBDanceAwardPage />
         </Suspense>
     )
 }
